0d7d14097218ae36e0ed7a3b5159e11d8b70111c
Maria Vovchuk <mari4ka62@gmail.com>
2023-02-25 18:02:37 +0200

    task6 done

---
+++ src/js/task-06.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('#validation-input')
+
+
+const onInputBlur = event => {
+    if ((Number(input.dataset.length))===event.currentTarget.value.length) {
+        input.classList.add('valid');
+        input.classList.remove('invalid');
+    } else {
+        input.classList.add('invalid');
+        input.classList.remove('valid');
+    }
+};
+
+input.addEventListener('blur', onInputBlur);
+
+ 
+// Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
+
+// <input
+//   type="text"
+//   id="validation-input"
+//   data-length="6"
+//   placeholder="Please enter 6 symbols"
+// />
+
+// Яка кількість символів повинна бути в інпуті, зазначається в його атрибуті data-length.
+// Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
+ 

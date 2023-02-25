755bf0862cebef8c9ba6eca22b8a73174849e287
Maria Vovchuk <mari4ka62@gmail.com>
2023-02-26 00:03:20 +0200

    task8 half done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,39 @@
+const form = document.querySelector('.login-form');
 
+
+
+
+form.addEventListener('submit', onFormSubmit);
+
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    const formEl = event.currentTarget.elements;
+    console.log(formEl);
+    const email = formEl.email.value;
+    console.log(email);
+  
+
+
+
+    // const formEl = event.currentTarget.elements;
+    // const email = formEl.email.value;
+    // const password = formEl.password.value;
+
+//     const formData = {
+//         email,
+//         password,
+//     };
+
+//   if (email==='' || password==='') {
+//       alert('Please, fill all form fields');
+//   } else {
+//       console.log(formData);
+
+//   }
+
+//     form.reset();
+    
+     
+};
